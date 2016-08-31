db5817043d1bde1ba5deb105ab03f79c69bb365f
Tanja Bergmann <tabergma@gmail.com>
2016-08-31 09:58:39 +0200

    Update evaluation script.

--- evaluation.py
+++ evaluation.py
@@ -5,6 +5,10 @@ if __name__ == "__main__":
 
     groups = [('michael', 'stefan'), ('dominik', 'rice'), ('joseph','tim'), ('anja', 'max'), ('thorsten', 'sebastian')]
 
+    overall_total = 0
+    overall_agreement = 0
+    overall_ok = 0
+
     for group in groups:
         json_out_file_1 = "data/" + group[0] + "/output.json"
         json_out_file_2 = "data/" + group[1] + "/output.json"
@@ -18,6 +22,7 @@ if __name__ == "__main__":
 
         total = 0
         agreement = 0
+        error_agreement = 0
         uninformative = 0
         nonfactual = 0
         incoherent = 0
@@ -44,30 +49,49 @@ if __name__ == "__main__":
                 if a_rel['correct'] == b_rel['correct'] == 'True':
                     agreement += 1
                     ok += 1
-                    continue
 
-                if a_rel['correct'] == b_rel['correct'] == 'False':
+                elif a_rel['correct'] == b_rel['correct'] == 'False':
                     agreement += 1
 
+                    same_error = False
                     if a_rel['informative'] == b_rel['informative'] == 'False':
                         uninformative += 1
+                        same_error = True
                     if a_rel['coherent'] == b_rel['coherent'] == 'False':
                         incoherent += 1
+                        same_error = True
                     if a_rel['factual'] == b_rel['factual'] == 'False':
                         nonfactual += 1
+                        same_error = True
                     if a_rel['other'] == b_rel['other'] == 'False':
                         other += 1
+                        same_error = True
 
-                if a_rel['correct'] == b_rel['correct'] == 'none' and a_rel['not-sure'] == b_rel['not-sure'] == 'True':
+                    if same_error:
+                        error_agreement += 1
+
+                elif a_rel['correct'] == b_rel['correct'] == 'none' and a_rel['not-sure'] == b_rel['not-sure'] == 'True':
                     not_sure += 1
                     agreement += 1
 
         print("")
         print("Group" + str(group))
         print("Agreement: " + str(agreement) + "/" + str(total))
+        print("Error Agreement:" + str(error_agreement) + "/" + str(agreement - ok))
         print("Ok: " + str(ok))
         print("Uninformative: " + str(uninformative))
         print("Incoherent: " + str(incoherent))
         print("Non-Factual: " + str(nonfactual))
         print("Other Errors: " + str(other))
         print("Not Sure: " + str(not_sure))
+
+        overall_agreement += agreement
+        overall_ok += ok
+        overall_total += total
+
+
+    print("")
+    print("")
+    print("RESULT")
+    print("Agreement: " + str(overall_agreement) + "/" + str(overall_total))
+    print("Ok: " + str(overall_ok))
